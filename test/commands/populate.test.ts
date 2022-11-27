140e742925adbab38c865b249fcef783a5e84862
Benedikt Rötsch <benedikt@hashbite.net>
2022-11-27 02:57:39 +0100

    test: add basic test for populate command

--- test/commands/populate.test.ts
+++ test/commands/populate.test.ts
@@ -4,14 +4,8 @@ describe('populate', () => {
   test
   .stdout()
   .command(['populate'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['populate', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
+  .it('executes', ctx => {
+    expect(ctx.stdout).to.contain('prefabs-populated.xml')
   })
 })
+
